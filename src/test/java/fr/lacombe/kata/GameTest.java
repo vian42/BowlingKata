9bca2a7cc81c1a75d0478539f4e38ac1891f907e
LaCombeDuLionVert_4 <billy>
2019-02-13 16:58:49 +0100

    Create test with a game with one frame with second roll with a number of pins

--- src/test/java/fr/lacombe/kata/GameTest.java
+++ src/test/java/fr/lacombe/kata/GameTest.java
@@ -1,10 +1,14 @@
 package fr.lacombe.kata;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static fr.lacombe.kata.Frame.FrameBuilder.aFrame;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class GameTest {
 
     @Test
@@ -17,12 +21,25 @@ public class GameTest {
     }
 
     @Test
-    public void given_a_game_with_one_frame_with_one_roll_with_one_pin_should_return_one() {
-        Roll rollOne = new Roll(1);
+    @Parameters({"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+    public void given_a_game_with_one_frame_with_first_roll_knocking_down_a_number_of_pins_should_return_the_number(int pins) {
+        Roll rollOne = new Roll(pins);
         Roll rollZero = new Roll(0);
         Frame frame = aFrame(rollOne).withSecondRoll(rollZero).build();
         Game game = new Game();
         game.addFrame(frame);
-        assertThat(game.score()).isEqualTo(Score.valueOf(1));
+        assertThat(game.score()).isEqualTo(Score.valueOf(pins));
     }
+
+    @Test
+    @Parameters({"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+    public void given_a_game_with_one_frame_with_second_roll_knocking_down_a_number_of_pins_should_return_the_number(int pins) {
+        Roll rollOne = new Roll(0);
+        Roll rollZero = new Roll(pins);
+        Frame frame = aFrame(rollOne).withSecondRoll(rollZero).build();
+        Game game = new Game();
+        game.addFrame(frame);
+        assertThat(game.score()).isEqualTo(Score.valueOf(pins));
+    }
+
 }
