4a4e5c99b7629540a29c833d9578afe1334f8db6
Vianney MAYNADIER <lenovo_21@DESKTOP-DB4UJ4D>
2019-03-04 14:12:23 +0100

    refactoring of spare point bonus.

--- src/main/java/fr/lacombe/kata/Game.java
+++ src/main/java/fr/lacombe/kata/Game.java
@@ -2,21 +2,26 @@ package fr.lacombe.kata;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Stream;
 
 public class Game {
     private List<Frame> frames = new ArrayList<>();
 
     public Score score() {
         Score score = Score.valueOf(0);
-        for (int i = 0; i < frames.size(); i++) {
-            Frame frame = frames.get(i);
 
+        ListIterator<Frame> frameIterator = frames.listIterator();
+        while (frameIterator.hasNext()) {
+            Frame frame = frameIterator.next();
             Score addedScore = frame.computeScore();
             score = score.plus(addedScore);
 
-            if (frame.isSpare() && i + 1 < frames.size()) {
-                score = score.plus(frames.get(i + 1).getFirstRollPin());
+            if (frame.isSpare() && frameIterator.hasNext()) {
+                score = score.plus(frameIterator.next().getFirstRollPin());
+                frameIterator.previous();
             }
         }
         return score;
